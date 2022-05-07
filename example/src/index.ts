bdf5d82a635eaf9ed2c5465ce1b103a9483901b7
visualkhh <visualkhh@gmail.com>
2022-05-07 22:50:22 +0900

    jest test

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,10 +1,1 @@
-import {fromEvent, interval, Observable, of, Subscription, timer} from "rxjs";
-import "rxjs/add/operator/take";
-import "rxjs/add/operator/map";
-import {fromArray} from "rxjs/internal/observable/fromArray";
-import {debounceTime, map} from "rxjs/operators";
-
-// fromArray([1,2,3,4,5,6]).pipe(map(it => it+'..'), debounceTime(20000)).subscribe(it => {
-interval(1000).pipe(map(it => it+'..')).subscribe(it => {
-    console.log(it+"-----11--111111111a111111d-")
-})
+console.log('--')
---
+++ example/test/rxjs.test.ts
@@ -0,0 +1,29 @@
+import {of} from 'rxjs'
+import {mergeMap} from 'rxjs/operators'
+
+describe('Test', () => {
+    test('rxjs test', (done) => {
+        // const response = await request(App.app).get('/');
+        // map(x => x * x)(of(1, 2, 3)).subscribe((v) => console.log(`value: ${v}`))
+
+        of([1, 2, 3]).pipe(
+            // tap(x => console.log(x))
+            mergeMap(it => of([5, 5]))
+        ).subscribe(it => {
+            console.log('xxx->', it)
+        })
+        // of(1, 2, 3).subscribe(it => {
+        //     console.log(it)
+        // })
+        // of([1, 2, 3]).subscribe(it => {
+        //     console.log(it)
+        // })
+        // from([1, 2, 3]).subscribe(it => {
+        //     console.log(it)
+        // })
+
+        expect(200).toBe(200)
+        done()
+    })
+})
+
